9fea5de6cc3665148f1e2de4d529cfaf02f16ae4
sodiqlef <kehindeogundimu731@gmail.com>
2022-11-20 16:02:52 +0100

    adding gitignore

--- eurobet/settings.py
+++ eurobet/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s@jjy+=)kf$yr*7z+#!e@x$clxw6k)4seta#_lv-xrh)dbnlys'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
